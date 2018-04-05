b7cb2c6b32311dc1e95a2bd432c35bb818afd87e
fabiocataldo <fabioalbertocataldo@gmail.com>
2018-04-05 12:56:13 -0300

    commit 2 5042018

--- TP5/mpi_find.c
+++ TP5/mpi_find.c
@@ -50,48 +50,63 @@ void read_file(char* nomb_file, int* vector, int size){
 }
 
 int main(int argc, char* argv[]){
-	int valor_a_buscar = atoi(argv[1]);
-	char* nombre_archivo = argv[2];
-
-	int tamanio_vec = obtener_tamanio(nombre_archivo);
-	int* valores = (int*)malloc(tamanio_vec*sizeof(int));
-	read_file(nombre_archivo, valores, tamanio_vec);
-
-	int rank, p, src, dst, tag=0, srcmain;
-	int mensajereceiv; //inicializo el lugar a donde va a el resultado del find()	
-	double start1,end1,t1;
-	MPI_Status status;
-	MPI_Request *request;
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-	if(rank!=0){  //rangos de los otros procesos 
-		
-		for (src = 1; src < p; src++ ) {
-			srcmain=0;
-			dst=srcmain;
-			MPI_Irecv(&valor_a_buscar, 1, MPI_INT, srcmain, tag, MPI_COMM_WORLD, &status);
-			mensajereceiv=find(valor_a_buscar, valores, tamanio_vec);
-			//printf("%d\n", mensajereceiv);
-			MPI_Isend(&mensajereceiv, 1, MPI_INT, dst, tag, MPI_COMM_WORLD, &request);
-		}
-		
-	}	
-	else{//rango del main
-		start1 = MPI_Wtime(); 
-		dst=1; //el numero del rango del primer proceso, por lo menos, es 1, los demás son >1
-		src=1;//el numero del rango del primer proceso, por lo menos, es 1, los demás son >1
-		MPI_Isend(&valor_a_buscar, 1, MPI_INT, dst, tag, MPI_COMM_WORLD, &request);
-		MPI_Irecv(&mensajereceiv, 1, MPI_INT, src, tag, MPI_COMM_WORLD, &status);
-		printf("%d\n", mensajereceiv);
-		end1 = MPI_Wtime();
-		t1=end1-start1;
-		printf("%lf\n",t1);
+	if(argv[1] == NULL && argv[2] == NULL){
+		fprintf(stderr, "Argumentos inválidos\n");
+		return 1;
 	}
+	if(argv[1] == NULL){
+		fprintf(stderr, "Numero de busqueda inválido\n");
+		return 1;
+	}	
+	if(argv[2] == NULL){
+		fprintf(stderr, "Nombre de archivo inválido\n");
+		return 1;
+	}	
+	else{
+		int valor_a_buscar = atoi(argv[1]);
+		char* nombre_archivo = argv[2];
 
-	MPI_Finalize();
-	free(valores);
-	return 0;
+		int tamanio_vec = obtener_tamanio(nombre_archivo);
+		int* valores = (int*)malloc(tamanio_vec*sizeof(int));
+		read_file(nombre_archivo, valores, tamanio_vec);
+
+		int rank, p, src, dst, tag=0, srcmain;
+		int mensajereceiv; //inicializo el lugar a donde va a el resultado del find()	
+		double start1,end1,t1;
+		MPI_Status status;
+		MPI_Request *request;
+
+		MPI_Init(&argc, &argv);
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+		if(rank!=0){  //rangos de los otros procesos 
+		
+			for (src = 1; src < p; src++ ) {
+				srcmain=0;
+				dst=srcmain;
+				MPI_Irecv(&valor_a_buscar, 1, MPI_INT, srcmain, tag, MPI_COMM_WORLD, &status);
+				mensajereceiv=find(valor_a_buscar, valores, tamanio_vec);
+				//printf("%d\n", mensajereceiv);
+				MPI_Isend(&mensajereceiv, 1, MPI_INT, dst, tag, MPI_COMM_WORLD, &request);
+			}
+		
+		}	
+		else{//rango del main
+			start1 = MPI_Wtime(); 
+			dst=1; //el numero del rango del primer proceso, por lo menos, es 1, los demás son >1
+			src=1;//el numero del rango del primer proceso, por lo menos, es 1, los demás son >1
+			MPI_Isend(&valor_a_buscar, 1, MPI_INT, dst, tag, MPI_COMM_WORLD, &request);
+			MPI_Irecv(&mensajereceiv, 1, MPI_INT, src, tag, MPI_COMM_WORLD, &status);
+			printf("%d\n", mensajereceiv);
+			end1 = MPI_Wtime();
+			t1=end1-start1;
+			printf("%lf\n",t1);
+		}
+
+		MPI_Finalize();
+		free(valores);
+		return 0;
+	}
+	
 }
