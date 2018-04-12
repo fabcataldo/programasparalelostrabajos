e047cffea7e41fff415951d2cc21f14b35ef0648
fabiocataldo <fabioalbertocataldo@gmail.com>
2018-04-12 15:48:44 -0300

    2 12042018

--- TP6/tp6_1.c
+++ TP6/tp6_1.c
@@ -35,23 +35,11 @@ void cargar_b(int *v, int n){
 int main(int argc, char *argv[]){
 	int p, rank, source=0, dest,tag=1, i,j,k;
 	int* a = NULL;
-	//int* b = NULL;
 
 	MPI_Status stat;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-	/*
-	MPI_Datatype porfilas;
-	//defino el nuevo tipo de datos "porfilas"
-	MPI_Type_contiguous(SIZEMAT, MPI_DOUBLE, &porfilas);
-	MPI_Type_commit(&porfilas);
-
-	//lleno el buffer de recepción
-	b = (int*)malloc(SIZEMAT*SIZEMAT*sizeof(int));
-	cargar_b(b, SIZEMAT);
-	*/
 
 	if (p == SIZEPROC) {
 		a = (int*)malloc(SIZEMAT*SIZEMAT*sizeof(int));
@@ -60,38 +48,20 @@ int main(int argc, char *argv[]){
 			printf("\nMatriz inicial\n");
 			cargar_matriz(a, SIZEMAT, SIZEMAT);
 			mostrarmatriz(a, SIZEMAT, SIZEMAT);
-			
 		}
 		//mando la matriz a todos los procesos, y éstos van a llamar a MPI_Bcast para recibir
 		MPI_Bcast(a, SIZEMAT*SIZEMAT, MPI_INT, 0, MPI_COMM_WORLD);  
 		
-		/*
-		for(k=0;k<2;k++){
-			for(i=0;i<9;i+2){
-				for(j=0;j<9;j++){
-					b[k*SIZEMAT+j]=a[i*SIZEMAT+j];
-
-
-			//mando 2 filas de 8 elementos a cada proceso, y cada proceso va a recibirlos en el buffer b
-			MPI_Scatter(&b, 1, MPI_DOUBLE, b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);	
-		}
-		*/
 		printf("\nContigua\n");
 		mostrarmatriz(a, SIZEMAT, SIZEMAT);
 
-		/*	printf("\nPor filas\n");
-			printf("\n%lf %lf \n",b[0], b[1]); //primer elemento, la fila i, y el otro elemento, la fila i+1				
-		*/
+		MPI_Finalize();
 	}
 	else{
 		printf("Must specify %d processors. Terminating.\n",SIZEPROC);
+		MPI_Finalize();
 	}
 	
 	free(a);
-	/*	
-	free(b);
-	MPI_Type_free(&porfilas);
-	*/
-	MPI_Finalize();
 	return 0;
 }
