a91aa236a543e797e144e08ec19cd101a3e51cb7
fabiocataldo <fabioalbertocataldo@gmail.com>
2018-05-17 13:00:38 -0300

    17052018

--- TP8/tp8.c
+++ TP8/tp8.c
@@ -8,22 +8,18 @@
 int size=4;
 int filas_grilla=2;
 int columnas_grilla=2;
-char* nomb_file="matrix.input";
+char* nomb_file="matrix_input";
 int np;
 
 void controlarargumentos(int argc, char** argv){
 	if(argc < 4){
-		printf("Debe ingresar al menos 3 argumentos de la siguiente forma:\n");
-		printf("mpirun -np <cantidad_de_procesos> nombre_ejecutable_programa <filas_grilla> <columnas_grilla> <size>\n");
-		printf("* <filas_grilla> e <columnas_grilla> se refieren al tamaño de las submatrices que surgen de la matriz (default: filas_grilla=2, columnas_grilla=2).\n");
-		printf("* <size> se refiere al tamaño de la matriz  (default: 4).\n");
+		fprintf(stderr, "Debe ingresar al menos 3 argumentos de la siguiente forma:\n");
+		fprintf(stderr, "mpirun -np <cantidad_de_procesos> nombre_ejecutable_programa <filas_grilla> <columnas_grilla> <size>\n");
+		fprintf(stderr, "* <filas_grilla> e <columnas_grilla> se refieren al tamaño de las submatrices que surgen de la matriz (default: filas_grilla=2, columnas_grilla=2).\n");
+		fprintf(stderr, "* <size> se refiere al tamaño de la matriz  (default: 4).\n");
 		exit(1);
 	}
-	if(np>4){
-		printf("Debe especificar una cantidad de procesos menor o igual a 4.");		
-		exit(1);
-	}
-	
+
 	if(filas_grilla!=0){
 		filas_grilla=atoi(argv[1]);
 	}
@@ -35,25 +31,21 @@ void controlarargumentos(int argc, char** argv){
 	}
 
 	if(size<2){
-		printf("El tamaño de las matrices tiene que ser mayor o igual a 2.\n");	
+		fprintf(stderr, "El tamaño de las matrices tiene que ser mayor o igual a 2.\n");	
 		exit(1);
 	}
 	if(filas_grilla>size || columnas_grilla>size){
-		printf("Algunas de las dimensiones de las submatrices es mayor al tamaño de la matriz.\n");	
+		fprintf(stderr, "Alguna de las dimensiones de las submatrices es mayor al tamaño de la matriz.\n");	
 		exit(1);		
 	}
 
 	if(filas_grilla!=columnas_grilla){
-		printf("Este programa multiplica matrices cuadradas. Las dimensiones de las submatrices son distintas, esto se traduce a matrices no cuadradas. Las dimensiones especificadas son inválidas.\n");	
+		fprintf(stderr, "Este programa multiplica matrices cuadradas. Las dimensiones de las submatrices son distintas. Luego, las dimensiones especificadas son inválidas.\n");	
 		exit(1);
 	}
-	//casos por ejemplo de 2 matrices de 100x100, que si por ej uso un número de procesos igual a 4, dichas matrices se dividirían
-	//en forma razonable y entendible en 4 submatrices de 25x25, por eso esta restricción
-	if(size>=10 && np>=4 && np<size){
-		if(filas_grilla!=np && columnas_grilla!=np){
-			printf("Dimensiones inválidas. Lo que se recomienda es que éstas sean iguales a la cantidad de procesos especificada, ya que luego se van a utilizar para armar las submatrices correspondientes.\n");	
-			exit(1);	
-		}	
+	if(np/2!=filas_grilla && np/2!=columnas_grilla){
+			fprintf(stderr, "Se recomienda, a fines de conseguir resultados correctos, escribir valores que sean la mitad de la cantidad de procesos especificada.\n");	
+			exit(1);
 	}
 }
 
@@ -80,47 +72,32 @@ void versubmatriz(int *m, int f, int c){
 void read_file(char* nomb_file, int* m, int* m2, int size){
 	FILE *fp = fopen(nomb_file, "r");
 	int i,j;
-	if (fp == NULL)
-		return;
+	if (fp == NULL){
+		fprintf(stderr, "El archivo 'matrix_input' no se encuentra en el directorio actual. \n");	
+		exit(1);
+	}
 
 	for(i=0;(i<size);i++){
 		for(j=0;j<size;j++){
-			fscanf(fp, "%d", &(m[i*size+j]));
+			if(fgetc(fp)!=0){
+				fscanf(fp, "%d", &(m[i*size+j]));
+			}
 		}	
 	}
 	
 	for(i=0;((i<size) || (fgetc(fp) != EOF));i++){
 		for(j=0;j<size;j++){
-			fscanf(fp, "%d", &(m2[i*size+j]));
+			if(fgetc(fp)!=0){
+				fscanf(fp, "%d", &(m2[i*size+j]));
+			}
 		}	
 	}
 
 	fclose(fp);
 }
 
-void generar_input(char* nomb_file, int size){
-	FILE *ptrfile2=fopen(nomb_file,"w+");
-	int i,j;
-	int num;
-	for(i=0;i<size;i++){
-		for(j=0;j<size;j++){
-			num=1+rand()%(101-1);
-			fprintf(ptrfile2,"%d ", num);				
-		}
-		fprintf(ptrfile2, "\n");
-	}
-	for(i=0;i<size;i++){
-		for(j=0;j<size;j++){
-			num=1+rand()%(101-1);
-			fprintf(ptrfile2,"%d ", num);				
-		}
-		fprintf(ptrfile2, "\n");
-	}
-	fclose(ptrfile2);	
-}
-
 void generar_archivo(int* matriz, int size) {
-	FILE *ptrfile=fopen("matrix.output","w+");
+	FILE *ptrfile=fopen("matrix_input.output","w+");
 	int i,j;
 	for(i=0;i<size;i++){
 		for(j=0;j<size;j++){
@@ -161,27 +138,25 @@ int main(int argc, char **argv){
 	}
 	
 
-	int* matriz_1 = (int*)calloc(size*size,sizeof(int));
-	int* matriz_2 = (int*)calloc(size*size,sizeof(int));
-	int* matriz_res = (int*)calloc(size*size,sizeof(int));
+	int* matriz_1 = NULL;
+	int* matriz_2 = NULL;
+	int* matriz_res = NULL;
 	int* subm_1= (int*)calloc(FILMP*COLMP,sizeof(int));
 	int* subm_2= (int*)calloc(FILMP*COLMP,sizeof(int));
 	int* subm_res = (int*)calloc(FILMP*COLMP,sizeof(int));
 
-	double start_t=MPI_Wtime();
 	int i,j,src;
 
 	if(rank==0){
-		generar_input(nomb_file, size);
+		matriz_1 = (int*)calloc(size*size,sizeof(int));
+		matriz_2 = (int*)calloc(size*size,sizeof(int));
+		matriz_res = (int*)calloc(size*size,sizeof(int));
+
 		read_file(nomb_file, matriz_1, matriz_2, size);
 	}
 
-	//el proceso 0 envía a todos (incluso se envía a él mismo) la submatriz a calcular, junto con la submatriz resultado
-	MPI_Datatype submp, nsubmp;
-	MPI_Type_vector(FILMP, COLMP, size, MPI_INT, &submp);
-	MPI_Type_create_resized(submp, 0, COLMP*sizeof(int),&nsubmp);
-	MPI_Type_commit(&nsubmp);
-	int offst[np];
+	double 	start_t=MPI_Wtime();
+	int offst[np];	
 	offst[0]=0;
 	offst[1]=COLMP;
 	offst[2]=size*(size/COLMP);
@@ -191,7 +166,13 @@ int main(int argc, char **argv){
 			offst[i]=size*(size/COLMP)+(COLMP*i);
 		}
 	}
+	MPI_Datatype submp, nsubmp;
+	//el proceso 0 envía a todos (incluso se envía a él mismo) la submatriz a calcular, junto con la submatriz resultado
+	MPI_Type_vector(FILMP, COLMP, size, MPI_INT, &submp);
+	MPI_Type_create_resized(submp, 0, COLMP*sizeof(int),&nsubmp);
+	MPI_Type_commit(&nsubmp);
 
+	
 	if(rank==0){
 		for(i=0;i<np;i++){
 			MPI_Send(matriz_1+offst[i], 1, nsubmp, i, tag, MPI_COMM_WORLD);
@@ -211,35 +192,45 @@ int main(int argc, char **argv){
 	//Ahora, todos los procesos reciben las submatrices que le corresponden, en la matriz_2
 	MPI_Recv(subm_2, FILMP*COLMP, MPI_INT, 0, tag, MPI_COMM_WORLD, &sts);
 
+	MPI_Type_free(&nsubmp);
+
 	//luego, todos se ponen a calcular, llamando a matmul(), y almacenando el resultado en subm_res
 	matmul(subm_1, subm_2, subm_res, FILMP, COLMP);
 
-	
+
+	free(subm_1);
+	free(subm_2);
+
 	MPI_Send(subm_res, FILMP*COLMP, MPI_INT, 0, tag, MPI_COMM_WORLD);			
+
+
 	//RECIBO DATOS:
-	if(rank==0){
+	if(rank==0){		
+		free(matriz_1);
+		free(matriz_2);
+
 		for(i=0;i<FILMP;i++){
 			for(j=0;j<COLMP;j++){
 				matriz_res[i*size+j]=subm_res[i*COLMP+j];
 			}
-		}		
-		
+		}
+
+		MPI_Datatype res, nres;
+		MPI_Type_vector(FILMP, COLMP, 1, MPI_INT, &res);
+		MPI_Type_create_resized(submp, 0, COLMP*sizeof(int),&nres);
+		MPI_Type_commit(&nres);
+
 		for(src=1;src<np;src++){
-			MPI_Recv(matriz_res+offst[i], 1, nsubmp, src, tag, MPI_COMM_WORLD, &sts);
+			MPI_Recv(matriz_res+offst[src], 1, nres, src, tag, MPI_COMM_WORLD, &sts);
 		}	
-
+		MPI_Type_free(&nres);
+		free(subm_res);				
 		printf("Tiempo transcurrido: %f\n", MPI_Wtime()-start_t);
-		generar_archivo(matriz_res, size);			
+		generar_archivo(matriz_res, size);	
+		
+		free(matriz_res);		
+
 	}
-
-	MPI_Type_free(&nsubmp);
-
-	free(matriz_1);
-	free(matriz_2);
-	free(matriz_res);
-	free(subm_1);
-	free(subm_2);
-	free(subm_res);
 
 	MPI_Finalize();
 	return 0;
