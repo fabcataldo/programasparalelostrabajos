1291d40a909502915cc490261b2c4009f6c78f9f
fabiocataldo <fabioalbertocataldo@gmail.com>
2018-06-08 22:06:48 -0300

    Mejoras en el archivo intpar.erl

--- TP-Erlang/intpar.erl
+++ TP-Erlang/intpar.erl
@@ -1,5 +1,5 @@
 -module(intpar).
--export([integral/3, compute_interval/7, compute_interval/6]).
+-export([integral/3, compute_interval_childs/7, compute_interval/6]).
 
 %%---------------------------------------------------------
 %% Función: integral/3
@@ -14,12 +14,11 @@ integral (F, A,B) ->
                                 Fa = F(A),
                                 Fb = F(B),
                                 Area = (Fa+Fb)*(B-A)/2,
-				Self=self(),
-                                Integral = compute_interval(F, A,B,Fa,Fb,Area,Self),
+                                Integral = compute_interval(F, A,B,Fa,Fb,Area),
                                 io:format("Integral de x^2: ~p~n",[Integral]).
 
-compute_interval (F, A, B, Fa, Fb, Area, Pid) -> 
-				%%io:format("AAAAAAAA~n"),
+%%función que ejecutan los hijos y "subhijos"
+compute_interval_childs (F, A, B, Fa, Fb, Area, Pid) ->
                                 M = (A+B)/2,
                                 Fm = F(M),
                                 Left_area = (Fa+Fm)*(M-A)/2,
@@ -27,15 +26,21 @@ compute_interval (F, A, B, Fa, Fb, Area, Pid) ->
 
                                 Cmp_1=abs( Area - (Left_area + Right_area)),
 				Cmp_2=math:pow(1.0, math:exp(-9)),
+				Pid_child = self(),
+				io:format("HIjo pid: ~w~n", [Pid_child]),
                                 if
                                    Cmp_1 < Cmp_2  ->   Pid ! Left_area+Right_area;
 
                                 true ->
-                                        Left = compute_interval (F,A,M,Fa,Fm,Left_area),
-                                        Right = compute_interval (F,M,B,Fm,Fb,Right_area),
-                                        Pid ! Left+Right
+                                        spawn(intpar, compute_interval_childs, [F, A, M, Fa, Fm, Left_area, Pid_child]),
+                                        Right = compute_interval_childs(F, M, B, Fm, Fb, Right_area, Pid_child),
+					receive Ihijo when is_float(Ihijo) ->
+						Left = Ihijo
+					end,                                        
+					Pid ! Left+Right
                                 end.
 
+%%función que ejecuta el MASTER
 compute_interval (F, A, B, Fa, Fb, Area) ->
                                 M = (A+B)/2,
                                 Fm = F(M),
@@ -45,11 +50,12 @@ compute_interval (F, A, B, Fa, Fb, Area) ->
                                 Cmp_1=abs( Area - (Left_area + Right_area)),
 				Cmp_2=math:pow(1.0, math:exp(-9)),
 				Self=self(),
+				io:format("Padre pid: ~w~n", [Self]),
                                 if
                                    Cmp_1 < Cmp_2 -> Left_area+Right_area;
                                 true ->
 						%%Si soy la tarea padre, creo mi tarea hija, y sigo calculando
-						spawn(intpar, compute_interval, [F,A,M,Fa,Fm,Left_area,Self]),
+						spawn(intpar, compute_interval_childs, [F,A,M,Fa,Fm,Left_area,Self]),
 						Right = compute_interval (F,M,B,Fm,Fb,Right_area),
 						%%cuando el otro proceso termine de calcular y me devuelva el subintervalo ya calculado
 						receive I when is_float(I) -> 
